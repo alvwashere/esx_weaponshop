73078d132e8ed4b49322a7f48f5cdb3409ca4b0d
Alv#9999 <72032190+alvwashere@users.noreply.github.com>
2023-02-25 15:24:49 +0000

    fxmanifest.lua
    Resource Manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Alv'
+description 'MenuV/OX Lib Weapon Shop'
+
+shared_scripts {
+  '@menuv/MenuV.lua',
+  --'@ox_lib/init.lua',
+  '@es_extended/imports.lua'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
